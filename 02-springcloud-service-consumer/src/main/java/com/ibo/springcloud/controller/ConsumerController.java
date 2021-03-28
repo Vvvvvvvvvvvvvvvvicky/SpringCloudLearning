aee02e795aaece8a08712c49f74e65fc36f58c92
vic <vicky.h.m.lee@outlook.com>
2021-03-28 23:15:57 +0800

    first commit

---
+++ 02-springcloud-service-consumer/src/main/java/com/ibo/springcloud/controller/ConsumerController.java
@@ -0,0 +1,22 @@
+package com.ibo.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class ConsumerController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @RequestMapping("/web/hello")
+    public String hello() {
+        //手动指定（需要配置ip、端口）
+//        return restTemplate.getForObject("http://localhost:8081/service/hello", String.class);
+
+        //Eureka
+        return restTemplate.getForObject("http://PROVIDER/service/hello", String.class);
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+01-springcloud-service-provider
+服务提供者
+
+02-springcloud-service-consumer
+服务消费者
+
+03-springcloud-eureka-server
+服务注册中心
